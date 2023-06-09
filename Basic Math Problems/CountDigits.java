7faa17f4c5c2b4d3c5e8d7902b4016e2d7e47563
an0nym0usraven <aakanksha.priya2020@vitbhopal.ac.in>
2023-06-09 19:52:34 +0530

    Create CountDigits.java

---
+++ Basic Math Problems/CountDigits.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class CountDigits
+{
+    //Approach 1 [O(log(n)) where n is number of digits in the given number]
+    static int count(int n)
+    {
+        int count = 0;
+        while(n!=0)
+        {
+            n /= 10;
+            count++;
+        }
+        return count;
+    }
+
+    //Approach 2 [O(1)]
+    static int count_2(int n)
+    {
+        String num = Integer.toString(n);
+        return num.length();
+    }
+
+    //Approach 3[O(1)]
+    static int count_3(int n)
+    {
+        int len = (int) (Math.floor(Math.log10(n)) + 1);
+        return len;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.println(count(n));
+        // System.out.println(count_2(n));
+        // System.out.println(count_3(n));
+        sc.close();
+    }
+}
