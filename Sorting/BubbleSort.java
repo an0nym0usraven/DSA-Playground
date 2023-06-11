f5878a81c964f7661fb1274e13d958146d49644a
an0nym0usraven <aakanksha.priya2020@vitbhopal.ac.in>
2023-06-11 23:44:59 +0530

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -6,6 +6,7 @@ public class BubbleSort {
     {
         for(int i=0; i<arr.length; i++)
         {
+            boolean swapped = false;
             for(int j=1; j<arr.length-i; j++)
             {
                 if(arr[j]<arr[j-1])
@@ -13,12 +14,18 @@ public class BubbleSort {
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
+                    swapped = true;
                 }
+            }
+
+            if(!swapped)
+            {
+                break;
             }
         }
     }
     public static void main(String[] args) {
-        int[] array = {3,5,2,6,7,1};
+        int[] array = {-45,23,0,26,9,-10};
         bubble(array);
         System.out.println(Arrays.toString(array));
     }
