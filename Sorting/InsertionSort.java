6b6b1577d72bc1cd2fc9bf285beb610d8a4e0ca0
an0nym0usraven <aakanksha.priya2020@vitbhopal.ac.in>
2023-06-12 00:47:19 +0530

    Create InsertionSort.java

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    static void insertion(int[] arr)
+    {
+        
+    }
+    public static void main(String[] args) {
+        int[] array = {-45,23,0,26,9,-10};
+        insertion(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
