d30de7c34ba784802b293876795f4a0d36b20bb1
an0nym0usraven <aakanksha.priya2020@vitbhopal.ac.in>
2023-06-11 23:40:03 +0530

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
     {
         for(int i=0; i<arr.length; i++)
         {
-            for(int j=1; j<arr.length-i; i++)
+            for(int j=1; j<arr.length-i; j++)
             {
                 if(arr[j]<arr[j-1])
                 {
