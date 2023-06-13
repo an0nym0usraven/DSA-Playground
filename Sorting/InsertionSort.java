3b479a44b57806626e6181debcc28520a5127612
an0nym0usraven <aakanksha.priya2020@vitbhopal.ac.in>
2023-06-13 11:31:08 +0530

    Update InsertionSort.java

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -3,7 +3,22 @@ import java.util.Arrays;
 public class InsertionSort {
     static void insertion(int[] arr)
     {
-        
+        boolean swapped = false;
+        for(int i=0; i<arr.length-1; i++)
+        {
+            for(int j=i+1; j>0; j--)
+            {
+                if(arr[j]<arr[j-1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+                if(!swapped)
+                break;
+            }
+        }
     }
     public static void main(String[] args) {
         int[] array = {-45,23,0,26,9,-10};
