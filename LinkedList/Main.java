7ee5c3b5982d239c6d2ee35c6d6e72d5c5793df3
an0nym0usraven <aakanksha.priya2020@vitbhopal.ac.in>
2023-06-22 13:46:18 +0530

    Update Main.java

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -4,19 +4,34 @@ package LinkedList;
 
 public class Main {
      public static void main(String[] args) {
+       
         LinkedList l = new LinkedList();
         l.insertAtBeginning(8);
         l.insertAtEnd(5);
+        l.insertAtEnd(9);
+        l.insertAtEnd(0);
         l.insert(2, 0);
         l.display();
 
         l.delete(2);
         l.display();
+        System.out.println("Maximum Element: " + l.max());
+        System.out.println("Index: " + l.findIndex(9));
 
-        l.deleteFromEnd();
-        l.display();
+        // l.deleteFromEnd();
+        // l.display();
         
-        l.deleteFromBeginning();
-        l.display();
+        // l.deleteFromBeginning();
+        // l.display();
+       
+
+       /* 
+       DoublyLinkedList dl = new DoublyLinkedList();
+       dl.insertAtBeginning(1);
+       dl.insertAtBeginning(9);
+       dl.insertAtBeginning(8);
+       dl.insertAtEnd(10);
+       dl.display();
+       */
     }
 }
